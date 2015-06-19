6ab661ebea009827ae3b8599964717ddab14ee27
Alex Zatsepin <Alex@MacBook-Air-Anne-Zatsepina.local>
2015-06-19 16:09:49 +0300

    Members don't work

--- views/pages/members.php
+++ views/pages/members.php
@@ -1,23 +1,30 @@
+<script language="JavaScript">
+    alert("Страница временно не доступна");
+    window.history.back(-1);
+</script>
+
 <?php
+/*
 $i=0;
 $j=0;
+
 $dirs=array();
 if ($handle =opendir($path)) {
-    while(false !==($dir=readdir($handle))){
-        $i+=1;
-        if ($i>2){
-            array_push($dirs, $dir);
-        }
+while(false !==($dir=readdir($handle))){
+    $i+=1;
+    if ($i>2){
+        array_push($dirs, $dir);
     }
+}
 }
 for ($j=0 ;$j<=count($dirs);$j++){
 ?>
-    <div class="text">
-        <div class="person">
-        <img src="<?echo $path."/".$dirs[$j]."/photo.jpg"; ?>" class="portrait">
-        <div class="description"><? echo $dirs[$j];?></div>
-    </div>
+<!--<div class="text">
+    <div class="person">
+    <img src="<?echo $path."/".$dirs[$j]."/photo.jpg"; ?>" class="portrait">
+    <div class="description"><? echo $dirs[$j];?></div>
 </div>
+</div>-->
 <?
 }
-?>
+?>*/
